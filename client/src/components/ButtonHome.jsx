7437da78fc96f39892940ca93436ba37e0c46e47
AXEL JANIAK <axeljpro@gmail.com>
2024-08-29 13:48:18 +0200

    fix

--- client/src/components/ButtonHome.jsx
+++ client/src/components/ButtonHome.jsx
@@ -1,9 +1,10 @@
 import { Link } from "react-router-dom";
+import arrowProject from "../assets/images/ArrowProject.svg";
 
 function ButtonHome() {
     return (
       <Link to="/Project/1">
-          <img src="../src/assets/images/ArrowProject.svg" alt="Vers les projets" className="animate__animated animate__tada"/>
+          <img src={arrowProject} alt="Vers les projets" className="animate__animated animate__tada"/>
       </Link>
     );
   }
